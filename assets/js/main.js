cb6a92174f606e87075f501c3c5e556f228e37d3
Ibrohiim <58775218+Ibrohiim@users.noreply.github.com>
2023-08-08 21:23:29 +0700

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -85,53 +85,6 @@
   }
 
   /**
-   * Mobile nav toggle
-   */
-  on("click", ".mobile-nav-toggle", function (e) {
-    select("#navbar").classList.toggle("navbar-mobile");
-    this.classList.toggle("bi-list");
-    this.classList.toggle("bi-x");
-  });
-
-  /**
-   * Mobile nav dropdowns activate
-   */
-  on(
-    "click",
-    ".navbar .dropdown > a",
-    function (e) {
-      if (select("#navbar").classList.contains("navbar-mobile")) {
-        e.preventDefault();
-        this.nextElementSibling.classList.toggle("dropdown-active");
-      }
-    },
-    true
-  );
-
-  /**
-   * Scrool with ofset on links with a class name .scrollto
-   */
-  on(
-    "click",
-    ".scrollto",
-    function (e) {
-      if (select(this.hash)) {
-        e.preventDefault();
-
-        let navbar = select("#navbar");
-        if (navbar.classList.contains("navbar-mobile")) {
-          navbar.classList.remove("navbar-mobile");
-          let navbarToggle = select(".mobile-nav-toggle");
-          navbarToggle.classList.toggle("bi-list");
-          navbarToggle.classList.toggle("bi-x");
-        }
-        scrollto(this.hash);
-      }
-    },
-    true
-  );
-
-  /**
    * Animation on scroll
    */
   window.addEventListener("load", () => {
@@ -252,6 +205,17 @@ function loadHeader() {
           link.classList.add("active");
         }
       });
+
+      // Menambahkan event listener untuk tombol mobile-nav-toggle
+      const mobileNavToggle =
+        headerContainer.querySelector(".mobile-nav-toggle");
+      const navbar = headerContainer.querySelector("#navbar");
+
+      mobileNavToggle.addEventListener("click", function () {
+        navbar.classList.toggle("navbar-mobile");
+        mobileNavToggle.classList.toggle("bi-list");
+        mobileNavToggle.classList.toggle("bi-x");
+      });
     })
     .catch((error) => console.error("Error loading header:", error));
 }
@@ -277,8 +241,8 @@ function loadFooter() {
     .catch((error) => console.error("Error loading footer:", error));
 }
 
-// Memanggil fungsi loadHeader dan loadFooter saat dokumen selesai dimuat
-document.addEventListener("DOMContentLoaded", function() {
+// Memanggil fungsi loadHeader saat dokumen selesai dimuat
+document.addEventListener("DOMContentLoaded", function () {
   loadHeader();
   loadFooter();
 });
